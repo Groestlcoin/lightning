caa65d6c06d25cf7b0c578606f4ec0d62b0ddb30
dflate <dflate@noreply.com>
2019-07-16 18:25:18 +0000

    eclair <--> grs-c-lightning compat-mode
    use Groestl hash only for blocks and tx
    
    Signed-off-by: dflate <dflate@noreply.com>

--- bitcoin/shadouble.c
+++ bitcoin/shadouble.c
@@ -4,10 +4,16 @@
 #include <common/type_to_string.h>
 #include <common/utils.h>
 
-void sha256_double(struct sha256_double *shadouble, const void *p, size_t len)
+void sha256_double_groestl(struct sha256_double *shadouble, const void *p, size_t len)
 {
 	sha256(&shadouble->sha, memcheck(p, len), len);
 	groestlhash((void *)&shadouble->sha, (void *)p, len);
+}
+
+void sha256_double(struct sha256_double *shadouble, const void *p, size_t len)
+{
+	sha256(&shadouble->sha, memcheck(p, len), len);
+	sha256(&shadouble->sha, &shadouble->sha, sizeof(shadouble->sha));
 }
 
 void sha256_double_done(struct sha256_ctx *shactx, struct sha256_double *res)
