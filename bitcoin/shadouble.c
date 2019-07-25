ebbe575003e6d5d7344db8fe9a0dd746d0a1c71e
Gruve_p <gruve-p@users.noreply.github.com>
2019-07-25 21:17:05 +0200

    Use sha256_done instead of sha256_double_done

--- bitcoin/shadouble.c
+++ bitcoin/shadouble.c
@@ -4,11 +4,6 @@
 #include <common/type_to_string.h>
 #include <common/utils.h>
 
-void groestl512_double(struct sha256_double *shadouble, const void *p, size_t len)
-{
-	groestlhash((void *)&shadouble->sha, (void *)p, len);
-}
-
 void sha256_double(struct sha256_double *shadouble, const void *p, size_t len)
 {
 	sha256(&shadouble->sha, memcheck(p, len), len);
@@ -17,7 +12,12 @@ void sha256_double(struct sha256_double *shadouble, const void *p, size_t len)
 
 void sha256_double_done(struct sha256_ctx *shactx, struct sha256_double *res)
 {
-	/* jush finish to generate single sha for groestl tx*/
 	sha256_done(shactx, &res->sha);
+	sha256(&res->sha, &res->sha, sizeof(res->sha));
+}
+
+void groestl512_double(struct sha256_double *shadouble, const void *p, size_t len)
+{
+	groestlhash((void *)&shadouble->sha, (void *)p, len);
 }
 REGISTER_TYPE_TO_HEXSTR(sha256_double);
