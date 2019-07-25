58f04924f6b2d40548c0d9e4e20275f82e3b6f97
dflate <dflate@noreply.com>
2019-07-25 14:21:48 +0000

    remove unused function
    In general keep it simple simple.
    And don't make fixes evil complicated.
    Where one simple call to an established function is enough,
    
    Signed-off-by: dflate <dflate@noreply.com>

--- bitcoin/shadouble.c
+++ bitcoin/shadouble.c
@@ -15,9 +15,4 @@ void sha256_double(struct sha256_double *shadouble, const void *p, size_t len)
 	sha256(&shadouble->sha, &shadouble->sha, sizeof(shadouble->sha));
 }
 
-void sha256_double_done(struct sha256_ctx *shactx, struct sha256_double *res)
-{
-	/* jush finish to generate single sha for groestl tx*/
-	sha256_done(shactx, &res->sha);
-}
 REGISTER_TYPE_TO_HEXSTR(sha256_double);
--- bitcoin/shadouble.h
+++ bitcoin/shadouble.h
@@ -11,5 +11,4 @@ struct sha256_double {
 void groestl512_double(struct sha256_double *shadouble, const void *p, size_t len);
 void sha256_double(struct sha256_double *shadouble, const void *p, size_t len);
 
-void sha256_double_done(struct sha256_ctx *sha256, struct sha256_double *res);
 #endif /* LIGHTNING_BITCOIN_SHADOUBLE_H */
